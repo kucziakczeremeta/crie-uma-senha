62e2b35bbe79023b6cac119fe98122aa56328e00
kucziakczeremeta <140640702+kucziakczeremeta@users.noreply.github.com>
2025-09-03 14:17:04 -0300

    dvv

--- scrips.js
+++ scrips.js
@@ -21,3 +21,26 @@ function aumentaTamanho (){
     geraSenha();
 }
 const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+function geraSenha (){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+    let senha = ';';
+    for (let i = 0; i < tamanhoSenha; i++){
+        
+    }
+}
